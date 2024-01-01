76a4efc69c00bd369dbf0d340c2ddead2efd25e6
XiaoYun <15543495640m@sina.cn>
2024-01-01 23:50:18 +0800

    Site updated: 2024-01-01 23:50:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/14/hello-world/"];function toRandomPost(){
+var posts=["2023/12/14/hello-world/","2024/01/01/hexo/npm图床使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
